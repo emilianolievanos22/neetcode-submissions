2f1df7c1286c6b574e83c54cb2f036d7ef4d23bb
Emiliano Lievanos <emilianolievanos22@gmail.com>
2026-05-25 15:50:51 -0700

    Add: python-naming-conventions - submission-0

---
+++ Python For Beginners/python-naming-conventions/submission-0.py
@@ -0,0 +1,7 @@
+python_creation_date = "February, 1991"
+java_reaction_date = "May, 1995"
+javascript_creation_date = "December, 1995"
+
+print(python_creation_date)
+print(java_reaction_date)
+print(javascript_creation_date)
